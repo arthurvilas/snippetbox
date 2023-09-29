79436a4e00a34c389a20275c8d557dc976316d8f
Arthur Santos <arthurvilas5@gmail.com>
2023-09-28 22:42:32 -0300

    Create base application

---
+++ cmd/web/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"log/slog"
+	"net/http"
+	"os"
+)
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	logger.Info("starting server", "addr", *addr)
+
+	err := http.ListenAndServe(*addr, mux)
+	logger.Error(err.Error())
+	os.Exit(1)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/arthurvilas/snippetbox
+
+go 1.21.1
